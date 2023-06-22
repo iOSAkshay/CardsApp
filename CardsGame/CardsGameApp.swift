fa9722e7bfb9538e785f7ad3628bac949e38676d
anandane-c <anandane-c@illuminateed.net>
2023-06-22 11:16:08 +0530

    SwiftUI code 1st Commit
    Basic feature added

---
+++ CardsGame/CardsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsGameApp.swift
+//  CardsGame
+//
+//  Created by Akshay Nandane on 22/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CardsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardsGame/ContentView.swift
@@ -0,0 +1,85 @@
+//
+//  ContentView.swift
+//  CardsGame
+//
+//  Created by Akshay Nandane on 22/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var playerCard = "card2"
+    @State private var cpuCard = "card4"
+    @State private var playerScore = 1
+    @State private var cpuScore = 0
+    
+    var body: some View {
+        
+        ZStack {
+            
+            Image("background").ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                Button(action: {
+                    //Generate random number between 2 and 12
+                    let playerRandom = Int.random(in: 2...12)
+                    let cpuRandom = Int.random(in: 2...12)
+                    // Update card
+                    playerCard = "card"+String(playerRandom)
+                    cpuCard = "card"+String(cpuRandom)
+                    // Update score
+                    playerScore += 1
+                    cpuScore += 1
+                    
+                }, label: {
+                    Image("dealbutton")
+                })
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                }
+                Spacer()
+                
+            }
+            
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().preferredColorScheme(.dark)
+    }
+}
